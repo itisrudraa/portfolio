36ddf0a526f31144716a3660fa85ab823fd486b3
itisrudraa <merudra01@gmail.com>
2026-08-02 17:50:07 +0530

    style: change line spacing

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -5,7 +5,7 @@ function About(){
         <section id="about" className="pt-5">
             <div className="max-w-2xl px-4 mx-auto">
                 <h2 className="text-3xl font-bold">About</h2>
-                <p className="mt-6 text-sm text-gray-600 leading-relaxed">
+                <p className="mt-6 text-sm text-gray-600 leading-7">
                     {PORTFOLIO.summary}
                 </p>
             </div>
