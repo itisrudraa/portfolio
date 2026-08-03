1991526ad49db76a5908abbc6b6c3466036cda70
itisrudraa <merudra01@gmail.com>
2026-08-03 22:59:16 +0530

    style: change padding

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -2,7 +2,7 @@ import PORTFOLIO from "../data/portfolio";
 
 function About(){
     return (
-        <section id="about" className="pt-5">
+        <section id="about" className="py-5">
             <div className="max-w-2xl px-4 mx-auto">
                 <h2 className="text-3xl font-bold">About</h2>
                 <p className="mt-6 text-sm text-gray-600 leading-7">
