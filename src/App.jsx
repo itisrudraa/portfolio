886fc10ad1dc41717c078d74dcfe93384e887f00
itisrudraa <merudra01@gmail.com>
2026-08-03 22:59:42 +0530

    add skills section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import About from "./sections/About";
 import Hero from "./sections/Hero";
+import Skills from "./sections/Skills";
 
 function App(){
   return (
     <>
       <Hero />
       <About />
+      <Skills />
     </>
   );
 }
