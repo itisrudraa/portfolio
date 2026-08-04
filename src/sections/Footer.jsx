51c7b4da7bf459e776a233d96e52e6ecd74f1d9c
itisrudraa <merudra01@gmail.com>
2026-08-04 16:40:29 +0530

    refactor footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -8,7 +8,7 @@ function Footer(){
                 </div>
                 <h2 className="mt-6 text-5xl font-bold tracking-tight">Get in Touch</h2>
                 <p className="mt-5 max-w-2xl mx-auto text-xl text-gray-600 leading-relaxed">Want to chat or discuss opportunities? Just drop me an email and I'll respond whenever I can. I will ignore all soliciting..</p>
-                <p className="mt-5 text-lg text-gray-600 leading-relaxed">Email: merudra01@gmail.com</p>
+                <a href="mailto:merudra01@gmail.com" className="mt-6 inline-block text-lg font-medium text-black hover:underline" >merudra01@gmail.com </a>
             </div>
         </section>
     );
