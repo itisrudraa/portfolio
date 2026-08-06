2dd5abba8669003aa2d95ab733fd65c397b7677f
itisrudraa <merudra01@gmail.com>
2026-08-06 18:35:21 +0530

    add hackathon section layout

---
+++ src/sections/Hackathons.jsx
@@ -0,0 +1,12 @@
+
+function Hackathons(){
+    return(
+        <section className="py-8">
+            <div className="max-w-2xl mx-auto p-4 bg-red-300">
+                <h3>Hackathons</h3>
+            </div>
+        </section>
+    );
+}
+
+export default Hackathons;
